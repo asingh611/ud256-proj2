d902621828f003363d7fe235b7bb50737dd67d3b
Andy Singh <andy.singh@usc.edu>
2019-05-25 17:31:45 -0700

    Wrote explanations for all problems except Problem 3, still working on Problem 3 -- seem to have built the tree correctly - need to build the encoding mechanism

--- problem_3.py
+++ problem_3.py
@@ -1,6 +1,37 @@
 import sys
 
+
+# Add Node and Tree classes
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def set_left_child(self, node):
+        self.left = node
+
+    def set_right_child(self, node):
+        self.right = node
+
+    def get_left_child(self):
+        return self.left
+
+    def get_right_child(self):
+        return self.right
+
+    def __repr__(self):
+        return str(self.value)
+
+
+class Tree:
+    def _init_(self, root):
+        self.root = root
+
+
 def huffman_encoding(data):
+    # Need to decide what the results would be for the edge cases
+
     # Based on string, get frequency of each character
     character_frequency = dict()
     for character in data:
@@ -9,12 +40,51 @@ def huffman_encoding(data):
         else:
             character_frequency[character] = 1
 
-    # Sort by frequency
-    character_frequency_sorted = sorted(character_frequency.items(), key=lambda dictionary: dictionary[1])
+    # Sort by frequency (descending so that we can pop the least frequent)
+    # TODO: Check if you can use a sorted dictionary (need to review time complexity)
+    character_frequency_sorted = sorted(character_frequency.items(),
+                                        key=lambda dictionary: dictionary[1],
+                                        reverse=True)
+
+    # Store created nodes here
+    node_storage = dict()  # Contains <Character string, Node>
 
     # Follow algorithm for building tree
     # Reference: https://www.siggraph.org/education/materials/HyperGraph/video/mpeg/mpegfaq/huffman_tutorial.html
 
+    while len(character_frequency_sorted) >= 2:
+        # Get the two least frequent characters
+        least_frequent = character_frequency_sorted.pop()
+        next_least_frequent = character_frequency_sorted.pop()
+        parent_node = Node()
+
+        # If there is a node created for this character string already
+        if least_frequent[0] in node_storage:
+            # Set it equal to the existing child node
+            parent_node.set_left_child(node_storage[least_frequent[0]])
+        else:
+            # Otherwise create a new child node
+            parent_node.set_left_child(Node(least_frequent[0]))
+
+        # Do the same for the right child
+        if next_least_frequent[0] in node_storage:
+            parent_node.set_right_child(node_storage[next_least_frequent[0]])
+        else:
+            parent_node.set_right_child(Node(next_least_frequent[0]))
+
+        parent_node.value = parent_node.get_left_child().value + parent_node.get_right_child().value
+
+        parent_frequency_values = least_frequent[1] + next_least_frequent[1]
+
+        node_storage[parent_node.value] = parent_node
+        character_frequency_sorted.append((parent_node.value, parent_frequency_values))
+
+        character_frequency_sorted = sorted(character_frequency_sorted,
+                                            key=lambda frequency_tuple: frequency_tuple[1],
+                                            reverse=True)
+
+
+    # Now that you have two nodes left, create the tree with the root node having the two remaining nodes
 
     # Traverse tree and for each leaf, store key/value -> key = character; value = encoded
     # Encode string one character at a time
@@ -22,8 +92,10 @@ def huffman_encoding(data):
 
     pass
 
-def huffman_decoding(data,tree):
+
+def huffman_decoding(data, tree):
     pass
+
 
 if __name__ == "__main__":
     codes = {}
